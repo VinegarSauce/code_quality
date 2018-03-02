8ff4975f22b74fd25e50084ddfac266100ba171b
Jamais <johan.paasche@epitech.eu>
2018-03-02 16:31:58 +0100

    Adding LinkedList Package

---
+++ linkedlist/src/test/java/LinkedListTester.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+import LinkedList.*;
+
+public class LinkedListTester {
+
+    @Test
+    public void testInsertElement()
+    {
+        LinkedList<int> list_to_test = new LinkedList<int>();
+    }
+}
